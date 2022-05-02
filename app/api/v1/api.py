557e1f87c1c414b352cbf034e763a5a606814ebc
Elvis Chege <elvischege@icloud.com>
2022-05-02 05:48:56 +0200

    Clean up git

--- app/api/v1/api.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from .routers import image, text
-
-api_router = APIRouter()
-api_router.include_router(text.router, prefix="/text", tags=["Text Search"])
-api_router.include_router(image.router, prefix="/image", tags=["Image Search"])
