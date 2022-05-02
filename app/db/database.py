10b0bbd5c63634c482bd084cda81c1cf9c823813
Elvis Chege <elvischege@icloud.com>
2022-05-02 05:44:08 +0200

    Update

--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Skyre2004@localhost/waitech'
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
